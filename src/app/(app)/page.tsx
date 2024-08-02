18fe25861d65fcc2728fe91dfb16fa1b31ac758c
sameer1sheikh2mansoori3 <94507751+sameer1sheikh2mansoori3@users.noreply.github.com>
2024-08-02 10:45:11 +0530

    landing page ui completed

---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,90 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import {  Mail } from 'lucide-react'; // Assuming you have an icon for messages
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import Autoplay from 'embla-carousel-autoplay';
+import messages from '@/messages.json';
+import TypingAnimation from "@/components/magicui/typing-animation";
+
+import ShinyButton from "@/components/magicui/shiny-button";
+import { TextGenerateEffect } from "../../components/ui/text-generate-effect";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+import TextRevealByWord from '@/components/magicui/text-reveal';
+import { LampDemo } from './../../components/ui/lamp';
+import { useRouter } from 'next/router';
+export default function Home() {
+ 
+
+ 
+  return (
+    <>
+      {/* Main content */}
+      <main className="flex flex-col items-center justify-center flex-grow px-4 py-12 text-white bg-black md:px-24">
+        <section className="mx-auto text-center md:mb-12">
+          {/* <h1 className="text-3xl font-bold md:text-5xl">
+            Dive into the World of Anonymous Feedback
+          </h1>
+          <p className="mt-3 text-base md:mt-4 md:text-lg">
+            True Feedback - Where your identity remains a secret.
+          </p> */}
+          <TextRevealByWord text="Assignment submission is a headache for many of us and SUPER BORING !!!!."/>
+          <LampDemo/>
+          
+        </section>
+        <section className="mx-auto text-center text-white md:mb-12">
+        <TypingAnimation
+      className="text-4xl font-bold text-white dark:text-black"
+      text="Just create a account and leave everything on us"
+    />
+        </section>
+        <section className='mx-auto my-auto'>
+        <Link href="/sign-up">
+        <ShinyButton text="Join now" className="text-white bg-white"  />
+       </Link>
+        </section>
+        
+
+        {/* Carousel for Messages */}
+        <Carousel
+          plugins={[Autoplay({ delay: 2000 })]}
+          className="w-full max-w-lg md:max-w-xl"
+        >
+          <CarouselContent>
+            {messages.map((message, index) => (
+              <CarouselItem key={index} className="p-4">
+                <Card>
+                  <CardHeader>
+                    <CardTitle>{message.title}</CardTitle>
+                  </CardHeader>
+                  <CardContent className="flex flex-col items-start space-y-2 md:flex-row md:space-y-0 md:space-x-4">
+                    <Mail className="flex-shrink-0" />
+                    <div>
+                      <p>{message.content}</p>
+                      <p className="text-xs text-muted-foreground">
+                        {message.received}
+                      </p>
+                    </div>
+                  </CardContent>
+                </Card>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+      </main>
+
+      {/* Footer */}
+      <footer className="p-4 text-center text-white bg-gray-900 md:p-6">
+        © 2023 True Feedback. All rights reserved.
+      </footer>
+    </>
+  );
+}
---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,26 @@
+
+import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from '@/types/ApiResponse';
+import { Resend } from "resend";
+const resend = new Resend('re_MCNHdczt_5THb7LE67N7bveK8G4ZezdLK');
+
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    
+    const response = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: email,
+      subject: 'Mystery Message Verification Code',
+      react: VerificationEmail({ username, otp: verifyCode }),
+    });
+    
+    return { success: true, message: 'Verification email sent successfully.' };
+  } catch (emailError) {
+    console.error('Error sending verification email:', emailError);
+    return { success: false, message: 'Failed to send verification email.' };
+  }
+}
