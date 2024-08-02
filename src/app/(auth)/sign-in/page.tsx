49e4187a054229d5e838ed9a203d83733454e865
sameer1sheikh2mansoori3 <94507751+sameer1sheikh2mansoori3@users.noreply.github.com>
2024-08-02 18:48:47 +0530

    change the name

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -59,11 +59,11 @@ export default function SignInForm() {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-800">
+    <div className="flex items-center justify-center min-h-screen bg-gray-800">
       <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
         <div className="text-center">
-          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
-            Welcome Back to True Feedback
+          <h1 className="mb-6 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            Welcome Back to Malik
           </h1>
           <p className="mb-4">Sign in to continue your secret conversations</p>
         </div>
@@ -94,7 +94,7 @@ export default function SignInForm() {
             <Button className='w-full' type="submit">Sign In</Button>
           </form>
         </Form>
-        <div className="text-center mt-4">
+        <div className="mt-4 text-center">
           <p>
             Not a member yet?{' '}
             <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -106,7 +106,7 @@ export default function SignUpForm() {
       <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
         <div className="text-center">
           <h1 className="mb-6 text-4xl font-extrabold tracking-tight lg:text-5xl">
-            Join True Feedback
+            Join Malik
           </h1>
           <p className="mb-4">Sign up to start your anonymous adventure</p>
         </div>
